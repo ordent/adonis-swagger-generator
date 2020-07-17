e202eaa8310d726917d4ebaf8238a9386e11e7a0
Dimas Satrio <dimassrio@gmail.com>
2020-07-17 17:39:58 +0700

    fixing path

--- src/compile.js
+++ src/compile.js
@@ -1,8 +1,14 @@
 const fs = require("fs").promises;
-const options = require("../config/swagger.js");
 const swaggerJSDoc = require("swagger-jsdoc");
-
+const path = require("path");
 module.exports = async function index() {
+  let options = null;
+  try {
+    options = require(path.resolve("./config/swagger.js"));
+  } catch (e) {
+    options = require(path.resolve(__dirname + "../config/swagger.js"));
+  }
+
   const swaggerSpec = swaggerJSDoc(options.options);
   await fs.writeFile(
     `docs_generated/swagger.json`,
